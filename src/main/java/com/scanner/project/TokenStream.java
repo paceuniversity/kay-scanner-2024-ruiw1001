430e9003547fee421beb25689ccbd76002ef4ce3
ruiw1001 <wangrui021001@gmail.com>
2025-11-27 00:08:14 -0500

    Update TokenStream.java

--- src/main/java/com/scanner/project/TokenStream.java
+++ src/main/java/com/scanner/project/TokenStream.java
@@ -1,276 +1,77 @@
-package com.scanner.project;
+package com.kaylerrenslow.parser;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import com.kaylerrenslow.parser.tokenizer.Token;
+import com.kaylerrenslow.parser.tokenizer.Tokenizer;
 
-public class TokenStream {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-    private BufferedReader reader;
-    private boolean isEof = false;
-    private int lineNumber = 1;
-
-    public int getLineNumber() {
-        return lineNumber;
+public class TokenStream implements Iterable<Token> {
+    
+    private final List<Token> tokens;
+    private int position;
+    
+    public TokenStream(String input) {
+        this.tokens = new ArrayList<>();
+        this.position = 0;
+        tokenize(input);
     }
-
-    public TokenStream(String fileName) {
-        try {
-            this.reader = new BufferedReader(new FileReader(fileName));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private char readChar() {
-        try {
-            int nextCharInAscii = reader.read();
-            if (nextCharInAscii == -1) {
-                isEof = true;
-                return '\0';
-            }
-            char nextChar = (char) nextCharInAscii;
-            if (nextChar == '\n') {
-                lineNumber++;
-            }
-            return nextChar;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void skipWhiteSpace() {
-        char currChar = ' ';
-        try {
-            reader.mark(1);
-            int currCharInAscii = reader.read();
-            if (currCharInAscii == -1) {
-                isEof = true;
-                return;
-            }
-            currChar = (char) currCharInAscii;
-
-            while (isWhiteSpace(currChar) || isEndOfLine(currChar)) {
-                if (currChar == '\n') {
-                    lineNumber++;
-                }
-                reader.mark(1);
-                currCharInAscii = reader.read();
-                if (currCharInAscii == -1) {
-                    isEof = true;
-                    return;
-                }
-                currChar = (char) currCharInAscii;
-            }
-
-            reader.reset();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Token nextToken() {
-        Token currToken = new Token();
-        currToken.setType("Other");
-        currToken.setValue("");
-
-        if (isEof) {
-            currToken.setType("EOF");
-            return currToken;
-        }
-
-        skipWhiteSpace();
-
-        char nextChar = readChar();
-
-        while (!isEof && (isWhiteSpace(nextChar) || isEndOfLine(nextChar) || isCommentChar(nextChar))) {
-
-            if (isCommentChar(nextChar)) {
-                while (!isEof && !isEndOfLine(nextChar)) {
-                    nextChar = readChar();
-                }
-            } else {
-                break;
-            }
-            nextChar = readChar();
-        }
-
-        if (isEof) {
-            currToken.setType("EOF");
-            return currToken;
-        }
-
-        currToken.setValue(currToken.getValue() + nextChar);
-
-        char currChar = readChar();
-        skipWhiteSpace();
-
-        if (Character.isLetter(nextChar)) {
-            while (!isEof && (Character.isLetterOrDigit(currChar) || currChar == '_')) {
-                currToken.setValue(currToken.getValue() + currChar);
-                currChar = readChar();
-            }
-
-            currToken.setType("Identifier");
-
-            if (isKeyword(currToken.getValue())) {
-                currToken.setType("Keyword");
-            } else if (currToken.getValue().equals("True")
-                    || currToken.getValue().equals("False")) {
-                currToken.setType("Literal");
+    
+    private void tokenize(String input) {
+        Tokenizer tokenizer = new Tokenizer(input);
+        while (tokenizer.hasNext()) {
+            Token token = tokenizer.next();
+            if (token != null) {
+                tokens.add(token);
             }
         }
-        else if (Character.isDigit(nextChar)) {
-            while (!isEof && Character.isDigit(currChar)) {
-                currToken.setValue(currToken.getValue() + currChar);
-                currChar = readChar();
-            }
-            currToken.setType("Literal");
-        }
-        else if (isSeparator(nextChar)) {
-            currToken.setType("Separator");
-        }
-        else if (isOperator(nextChar)) {
-            currToken.setType("Operator");
-
-            switch (nextChar) {
-
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                    break;
-
-                case '<':
-                    if (currChar == '=') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    }
-                    break;
-
-                case '>':
-                    if (currChar == '=') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    }
-                    break;
-
-                case '=':
-                    if (currChar == '=') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    } else {
-                        currToken.setType("Other");
-                    }
-                    break;
-
-                case ':':
-                    if (currChar == '=') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    } else {
-                        currToken.setType("Other");
-                    }
-                    break;
-
-                case '!':
-                    if (currChar == '=') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    }
-                    break;
-
-                case '&':
-                    if (currChar == '&') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    } else {
-                        currToken.setType("Other");
-                    }
-                    break;
-
-                case '|':
-                    if (currChar == '|') {
-                        currToken.setValue(currToken.getValue() + currChar);
-                        currChar = readChar();
-                    } else {
-                        currToken.setType("Other");
-                    }
-                    break;
-
-                default:
-                    currToken.setType("Other");
-            }
-        }
-        else {
-            while (!isEof && !isEndOfToken(currChar)) {
-                currToken.setValue(currToken.getValue() + currChar);
-                currChar = readChar();
-            }
-        }
-
-        return currToken;
     }
-
-    private boolean isSeparator(char currChar) {
-        if (currChar == '(' || currChar == ')' ||
-            currChar == '{' || currChar == '}' ||
-            currChar == ',' || currChar == ';') {
-            return true;
-        } else {
-            return false;
-        }
+    
+    public boolean hasNext() {
+        return position < tokens.size();
     }
-
-    private boolean isOperator(char currChar) {
-        if (currChar == '+' || currChar == '-' || currChar == '*' ||
-            currChar == '/' || currChar == '<' || currChar == '>' ||
-            currChar == ':' || currChar == '!' || currChar == '=' ||
-            currChar == '&' || currChar == '|') {
-            return true;
-        } else {
-            return false;
+    
+    public Token next() {
+        if (!hasNext()) {
+            return null;
         }
+        return tokens.get(position++);
     }
-
-    private boolean isCommentChar(char currChar) {
-        return currChar == '/';
-    }
-
-    private boolean isEndOfToken(char currChar) {
-        if (isWhiteSpace(currChar) || isSeparator(currChar) ||
-            isOperator(currChar) || isCommentChar(currChar) ||
-            isEndOfLine(currChar)) {
-            return true;
-        } else {
-            return false;
+    
+    public Token peek() {
+        if (!hasNext()) {
+            return null;
         }
+        return tokens.get(position);
     }
-
-    private boolean isEndOfLine(char currChar) {
-        if (currChar == '\n' || currChar == '\r') {
-            return true;
-        } else {
-            return false;
-        }
+    
+    public void reset() {
+        position = 0;
     }
-
-    private boolean isWhiteSpace(char currChar) {
-        if (currChar == ' ' || currChar == '\t' || currChar == '\f') {
-            return true;
-        } else {
-            return false;
-        }
+    
+    public int getPosition() {
+        return position;
     }
-
-    private boolean isKeyword(String s) {
-        if (s.equals("bool") || s.equals("integer") ||
-            s.equals("if")   || s.equals("else")    ||
-            s.equals("main") || s.equals("while")) {
-            return true;
-        } else {
-            return false;
+    
+    public void setPosition(int position) {
+        if (position < 0 || position > tokens.size()) {
+            throw new IllegalArgumentException("Position out of bounds: " + position);
         }
+        this.position = position;
+    }
+    
+    public List<Token> getTokens() {
+        return new ArrayList<>(tokens);
+    }
+    
+    @Override
+    public Iterator<Token> iterator() {
+        return tokens.iterator();
+    }
+    
+    @Override
+    public String toString() {
+        return "TokenStream{tokens=" + tokens + ", position=" + position + "}";
     }
 }
